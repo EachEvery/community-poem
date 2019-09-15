13c17d1b8d5a90b8a345d7806a31578c83be8ec8
Nate Hobi <nate.hobi@gmail.com>
2019-09-15 00:04:35 -0400

    Fixes flare

--- config/logging.php
+++ config/logging.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'single'),
+    'default' => env('LOG_CHANNEL', 'stack'),
 
     /*
     |--------------------------------------------------------------------------
