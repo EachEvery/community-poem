66b4c3f3354462437d9a9e5fa40eed223432434f
Nate Hobi <nate.hobi@gmail.com>
2019-11-21 12:37:54 -0500

    Adds meta tag

--- resources/views/peacePoem.blade.php
+++ resources/views/peacePoem.blade.php
@@ -6,6 +6,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{csrf_token()}}">
 
+<meta name="apple-mobile-web-app-capable" content="yes">
+
 <link href="https://fonts.googleapis.com/css?family=Homemade+Apple|Work+Sans:300,400,500,600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://use.typekit.net/gow0spk.css">
 
