57d1d94bffe4690fceb2ee7d9ed31947a733f5ae
Joe Holston <joeholston@joes-mbp-2.lan>
2022-01-26 11:22:53 -0500

    updates click handlers for poem controls, resolves printing

--- resources/views/webResponses.blade.php
+++ resources/views/webResponses.blade.php
@@ -35,7 +35,6 @@
             <div class="container mx-auto grid mt-24 text-center ">
             
                 @foreach($space->approved_responses()->latest()->limit(100)->get() as $index => $response)
-                {{-- @foreach($space->approved_responses as $index => $response) --}}
                     @php
                         $isHighlighted = request('highlight') == strval($response->id);
                         $delay = $index > 15 ? 0 : $loop->index * 40; //ms
@@ -87,151 +86,159 @@
 {{-- SYNC WITH PEACE POEM RESPONSES --}}
 
 <script type="text/javascript">
-(function() {
-     setTimeout(() => {
-        var $grid = $('.grid').isotope({
-            itemSelector: '.response',
-                layoutMode: 'masonry',
-            });
-
-            $(window).on('scroll', function() {
-            var isAtBottom = $(window).scrollTop() + $(window).height() >= $(document).height();
-            if(isAtBottom) {
-                $('.loading-indicator').removeClass('opacity-0').addClass('opacity-100');
-                clearTimeout(window.infiniteScrollTimeout);
-                window.infiniteScrollTimeout = setTimeout(function() {
-                    $.get('/paged/responses?spaceId=' + $('.space-id').text() + '&offset=' + $('.response').length, function(res) {
-                        var $offsetResults = $(res);
-                        $grid.append( $offsetResults ).isotope( 'appended', $offsetResults );
-                        $('.loading-indicator').removeClass('opacity-100').addClass('opacity-0');
-                    });
-                }, 500);   
-            }
-        });
-
-        $('.response').css({'opacity': 1, 'transform': 'none'});
-
-        if(!$('.highlight').length) return;
-        
-        const tour = new Shepherd.Tour({
-                useModalOverlay: true,
-                
-                defaultStepOptions: {
-                    classes: ['font-display'],
-                    modalOverlayOpeningPadding: 20,
-                    modalOverlayOpeningRadius: 5,
-                    cancelIcon: {
-                        enabled: true
-                    },
-                    classes: 'class-1 class-2',
-                    scrollTo: { behavior: 'smooth', block: 'center' }
+    (function() {
+         setTimeout(() => {
+            var $grid = $('.grid').isotope({
+                itemSelector: '.response',
+                    layoutMode: 'masonry',
+                });
+    
+                $(window).on('scroll', function() {
+                var isAtBottom = $(window).scrollTop() + $(window).height() >= $(document).height();
+                if(isAtBottom) {
+                    $('.loading-indicator').removeClass('opacity-0').addClass('opacity-100');
+                    clearTimeout(window.infiniteScrollTimeout);
+                    window.infiniteScrollTimeout = setTimeout(function() {
+                        $.get('/paged/responses?spaceId=' + $('.space-id').text() + '&offset=' + $('.response').length, function(res) {
+                            var $offsetResults = $(res);
+                            $grid.append( $offsetResults ).isotope( 'appended', $offsetResults );
+                            $('.loading-indicator').removeClass('opacity-100').addClass('opacity-0');
+                        });
+                    }, 500);   
                 }
             });
-
-            tour.addStep({
-                title: 'Here\'s Your Poem!',
-                text: `Thanks for creating a poem for {{$space->name}}.`,
-                attachTo: {
-                    element: '.highlight',
-                    on: 'bottom'
-                },
-                buttons: [
-                    {
-                    action() {
-                        tour.cancel();
-                    },
-                    classes: 'shepherd-button-secondary',
-                    text: 'Dismiss'
-                    },
+    
+            $('.response').css({'opacity': 1, 'transform': 'none'});
+    
+            if(!$('.highlight').length) return;
+            
+            const tour = new Shepherd.Tour({
+                    useModalOverlay: true,
                     
-                ],
-                id: 'creating'
-            });
+                    defaultStepOptions: {
+                        classes: ['font-display'],
+                        modalOverlayOpeningPadding: 20,
+                        modalOverlayOpeningRadius: 5,
+                        cancelIcon: {
+                            enabled: true
+                        },
+                        classes: 'class-1 class-2',
+                        scrollTo: { behavior: 'smooth', block: 'center' }
+                    }
+                });
+    
+                tour.addStep({
+                    title: 'Here\'s Your Poem!',
+                    text: `Thanks for creating a poem for {{$space->name}}.`,
+                    attachTo: {
+                        element: '.highlight',
+                        on: 'bottom'
+                    },
+                    buttons: [
+                        {
+                        action() {
+                            tour.cancel();
+                        },
+                        classes: 'shepherd-button-secondary',
+                        text: 'Dismiss'
+                        },
+                        
+                    ],
+                    id: 'creating'
+                });
+    
+                tour.start();
+         }, 150);
+    })();
+    
+    (function() {
+         function resetSelected(parent, event) {
+            event.stopPropagation();
+            $(".response.selected").removeClass("selected");
+            $('.controls', parent).removeClass('rotate');
+            $('.controls .copy.success').removeClass("success");
+            $('.controls .share.success').removeClass("success");
+            $('.content.border-primary').removeClass('border-primary')
+        }
+    
+        $(window).click(function(event) {
+            resetSelected(this, event)
+        });
+    
+        $('.container').on('click', '.response', function(event){
+            resetSelected(this, event);
+            event.stopPropagation();
+            
+            $('.content', this).css('transition', '125ms ease-in all 0ms')
+    
+            var offset = $(this).offset(); 
+            var responseControls = $('.controls', this);
+            var relX = event.pageX - offset.left - (responseControls.width() / 2);
+            var relY = event.pageY - offset.top - (responseControls.height() / 2);
+            responseControls.css({"top": relY, "left": relX})
+    
+            setTimeout(() => {
+                $('.controls', this).addClass('rotate')
+            }, 50);
+    
+            $(this).addClass('selected');
+            $('.content', this).addClass('border-primary')
+        });
+    
+        $('.container').on('click', '.controls .close', function(event){
+            resetSelected(this, event)
+        });
+    
+        $('.container').on('click', '.controls .print', function(event){
+            event.stopPropagation();
+            $(this).addClass('loading');
+            var responseId = $(this).closest('.response').attr('id');
+            var responseToPrint = $('#print-'+responseId);
+            responseToPrint.show();
 
-            tour.start();
-     }, 150);
-})();
-
-// (function() {
-//      function resetSelected(parent, event) {
-//         event.stopPropagation();
-//         $(".response.selected").removeClass("selected");
-//         $('.controls', parent).removeClass('rotate');
-//         $('.controls .copy.success').removeClass("success");
-//         $('.controls .share.success').removeClass("success");
-//         $('.content.border-primary').removeClass('border-primary')
-//     }
-
-//     $(window).click(function(event) {
-//         resetSelected(this, event)
-//     });
-
-//     $('.response').click(function(event){
-//         resetSelected(this, event);
-//         event.stopPropagation();
+            setTimeout(() => {
+                html2canvas(responseToPrint[ 0 ], { scale: 2, useCORS: true }).then((canvas) => {
+                    var imageToPrint = canvas.toDataURL("image/png", 1.0);
+                    var inputCode = window.prompt("Enter Print Code");
+                    axios.post("https://ts-print.eachevery.dev/job", {
+                        content: imageToPrint,
+                        code: inputCode,
+                        origin: window.location.origin,
+                    })
+                        .then(() => {
+                            window.alert("Success! Printing Now...");
+                            responseToPrint.hide();
+                            $(this).removeClass('loading');
+                        })
+                        .catch((error) => {
+                            window.alert(error.response.data);
+                            $(this).removeClass('loading');
+                        });
+                })
+            }, 15);
+        });
+    
+        const copyToClipboard = (textToCopy) => {
+            var $temp = $("<input>");
+            $("body").append($temp);
+            $temp.val(textToCopy).select();
+            document.execCommand("copy");
+            $temp.remove();
+        };
         
-//         $('.content', this).css('transition', '125ms ease-in all 0ms')
-
-//         var offset = $(this).offset(); 
-//         var responseControls = $('.controls', this);
-//         var relX = event.pageX - offset.left - (responseControls.width() / 2);
-//         var relY = event.pageY - offset.top - (responseControls.height() / 2);
-//         responseControls.css({"top": relY, "left": relX})
-
-//         setTimeout(() => {
-//             $('.controls', this).addClass('rotate')
-//         }, 50);
-
-//         $(this).addClass('selected');
-//         $('.content', this).addClass('border-primary')
-//     });
-
-//     $('.controls .close').click(function(event) {
-//         resetSelected(this, event)
-//     });
-
-//     $('.controls .print').click(function(event) {
-//         event.stopPropagation();
-//         var responseId = $(this).closest('.response').attr('id');
-//         var responseToPrint = $('#print-'+responseId);
-//         responseToPrint.show();
-//         html2canvas(responseToPrint[ 0 ], { scale: 2, useCORS: true }).then((canvas) => {
-//             var imageToPrint = canvas.toDataURL("image/png", 1.0);
-//             var inputCode = window.prompt("Enter Print Code");
-//             axios.post("https://ts-print.eachevery.dev/job", {
-//                 content: imageToPrint,
-//                 code: inputCode,
-//                 origin: window.location.origin,
-//             })
-//                 .then(() => {
-//                     window.alert("Success! Printing Now...");
-//                     responseToPrint.hide();
-//                 })
-//                 .catch((error) => window.alert(error.response.data));
-//         });
-//     });
-
-//     const copyToClipboard = (textToCopy) => {
-//         var $temp = $("<input>");
-//         $("body").append($temp);
-//         $temp.val(textToCopy).select();
-//         document.execCommand("copy");
-//         $temp.remove();
-//     };
+        $('.container').on('click', '.controls .copy', function(event){
+            event.stopPropagation();
+            copyToClipboard($(this).closest('.response').find('.content .content-text').text());
+            $(this).addClass('success');
+        });
     
-//     $('.controls .copy').click(function(event) {
-//         event.stopPropagation();
-//         copyToClipboard($(this).closest('.response').find('.content .content-text').text());
-//         $(this).addClass('success');
-//     });
-
-//     $('.controls .share').click(function(event) {
-//         event.stopPropagation();
-//         copyToClipboard($(this).attr('data-share-link'));
-//         $(this).addClass('success');
-//     });
-// })();
-    
+        $('.container').on('click', '.controls .share', function(event){
+            event.stopPropagation();
+            copyToClipboard($(this).attr('data-share-link'));
+            $(this).addClass('success');
+        });
+    })();
+        
 </script>
 
 
